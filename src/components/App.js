f16f701adabf029853cc4ec0d3c406bbca04ce92
Basil <Vaseokk@gmail.com>
2020-09-01 20:42:44 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello there, we are going to build a Youtube Clone</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDymlAJbJ9h2p24MAzYXvZ9VpPNzikD4NI",
+  authDomain: "clone-dc283.firebaseapp.com",
+  databaseURL: "https://clone-dc283.firebaseio.com",
+  projectId: "clone-dc283",
+  storageBucket: "clone-dc283.appspot.com",
+  messagingSenderId: "582452415807",
+  appId: "1:582452415807:web:abf8e846441f4dac64578a",
+  measurementId: "G-RTLHMV82LS"
+};
